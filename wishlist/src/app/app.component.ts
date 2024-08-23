dd6b059c20242c2253fa8566325a0cc31df3685b
TuDinhVuu <dinhtu2904.dev@gmail.com>
2024-08-23 17:01:10 +0700

    Change

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { WishItem } from '../shared/models/wishItem';
 import { EventService } from '../shared/models/services/EventService';
-import {WishService} from './wish.service';
+import { WishService } from './wish.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent implements OnInit {
-  items! : WishItem[];
+  items : WishItem[] = [];
 
   constructor(events: EventService, private wishService: WishService) {
     events.listen('removeWish', (wish : any) => {
@@ -21,9 +20,14 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.wishService.getWishes().subscribe((data : any) => {
-      this.items = data;
-    })
+    this.wishService.getWishes().subscribe(
+      (data : any) => {
+        this.items = data;
+      },
+      (error : any) => {
+        alert(error.message);
+      }
+    );
   }
 
   
